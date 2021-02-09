7f26cf29977219d580c0b78e121c912e48972ec3
Johan Sjöberg <johan.sjoberg@stenaline.com>
2021-02-09 08:18:40 +0100

    updates

--- client/snowpack.config.js
+++ client/snowpack.config.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
 // Example: snowpack.config.js
 // The added "@type" comment will enable TypeScript type information via VSCode, etc.
 const httpProxy = require('http-proxy');
