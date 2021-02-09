db069568cf6e2fef1b3b0c1a6ef1bcd0f70f7c95
Johan Sjöberg <johan.sjoberg@stenaline.com>
2021-02-09 21:15:11 +0100

    Updates

--- dist/redux/types/pageTypes.js
+++
@@ -1,6 +0,0 @@
-export const ACTIVE_PAGE = "ACTIVE_PAGE";
-export const ADD_PAGE = "ADD_PAGE";
-export const REMOVE_PAGE = "REMOVE_PAGE";
-export const EDIT_PAGE = "EDIT_PAGE";
-export const RENAME_PAGE = "RENAME_PAGE";
-export const MENU_PAGE = "MENU_PAGE";
